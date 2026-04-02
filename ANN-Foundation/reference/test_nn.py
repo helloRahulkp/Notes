b1ecfc1995a827e41c973935798722d411f7c494
helloRahulkp <hello.rahulkp@gmail.com>
2026-04-02 19:34:26 +0530

    ANN Foundation

--- ANN-Foundation/reference/test_nn.py
+++
@@ -1,7 +0,0 @@
-from micrograd.nn import MLP
-
-
-def test_mlp_forward():
-    model = MLP(3, [4, 1])
-    out = model([1.0, 2.0, 3.0])
-    assert out is not None
---
+++ ANN-Foundation/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup() # It will automatically pull configuration from pyproject.toml
+
+# pip install -e ".[dev]" to use pyproject.toml
